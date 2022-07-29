14a6646bb59936f2b0400ac289491f35f907e9b6
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-07-28 21:24:10 -0300

    finalizado unitests 002

--- unitests 002/baconcomovos.py
+++ unitests 002/baconcomovos.py
@@ -12,16 +12,31 @@ TDD - TEST DRIVEN DEVELOPMENT - DESENVOLVIMENTO DIRIGIDO A TESTES
         PARTE 3 - MELHORAR O CÓDIGO
 
 
-1 - RECEBER UM NÚMERO INTEIRO;
-2 - SABER SE O NUM É MULT DE 3 E 5
+1 - RECEBER UM NÚMERO INTEIRO; OK
+2 - SABER SE O NUM É MULT DE 3 E 5 OK
         RETORNAR 'BACON COM OVOS';
-3 - SABER SE O NÚMERO NÃO É MULT DE 3 E 5
+3 - SABER SE O NÚMERO NÃO É MULT DE 3 E 5 OK
         RETORNAR 'PASSAR FOME';
-4 - SABER SE O NÚMERO É MULT SOMENTE DE 3
+4 - SABER SE O NÚMERO É MULT SOMENTE DE 3 OK
         RETORNAR 'BACON';
 5 - SABER SE O NÚMERO É MULT SOMENTE DE 5
         RETORNAR 'OVOS'.
 """
 
 def bacon_com_ovos(n):
-    assert isinstance(n, int), 'n deve ser um int'
+        assert isinstance(n, int), 'n deve ser um int'
+    
+        if n % 3 == 0 and n % 5 == 0:
+                return 'Bacon com Ovos'
+
+        if n % 3 != 0 and n % 5 != 0:
+                return 'Passar Fome'
+
+        if n % 3 == 0 and n % 5!= 0:
+                return 'Bacon'
+        
+        if n % 3 != 0 and n % 5 == 0:
+                return 'Ovos'
+        
+print(bacon_com_ovos('1'))
+
--- unitests 002/test_baconcomovos.py
+++ unitests 002/test_baconcomovos.py
@@ -9,10 +9,58 @@ MELHORAR O CÓDIGO.
 
 class TestBaconComOvos(unittest.TestCase):
     
-    def test_deve_receber_um_numero_inteiro_senao_retorna_assertionerror(self):
+    def test_bacon_com_ovos_deve_receber_um_numero_inteiro_senao_retorna_assertionerror(self):
         with self.assertRaises(AssertionError):
             bacon_com_ovos('str')
 
+    def test_bacon_com_ovos_deve_retorna_bacon_com_ovos_se_numero_multiplo_de_3_e_5(self):
+        entradas = (15, 30, 45, 60)
+        saida = 'Bacon com Ovos'
+        
+        for entrada in entradas:
+            with self.subTest(entrada=entrada, saida=saida):
+                self.assertEqual(
+                    bacon_com_ovos(entrada), 
+                    saida,
+                    msg = f'"{entrada}" não retornou "{saida}"'
+                    )
+
+    def test_bacon_com_ovos_deve_retornar_passar_fome_se_numero_nao_for_multiplo_de_3_e_5(self):
+        entradas = (2, 4, 7, 8, 11)
+        saida = 'Passar Fome'
+        
+        for entrada in entradas:
+            with self.subTest(entrada=entrada, saida=saida):
+                self.assertEqual(
+                    bacon_com_ovos(entrada), 
+                    saida,
+                    msg = f'"{entrada}" não retornou "{saida}"'
+                    )
+
+    def test_bacon_com_ovos_deve_retornar_bacon_se_numero_for_multiplo_somente_de_3(self):
+        entradas = (3, 6, 9, 12)
+        saida = 'Bacon'
+        
+        for entrada in entradas:
+            with self.subTest(entrada=entrada, saida=saida):
+                self.assertEqual(
+                    bacon_com_ovos(entrada), 
+                    saida, 
+                    msg = f'"{entrada}" não retornou "{saida}"'
+                    )
+
+
+    def test_bacon_com_ovos_deve_retornar_ovos_se_numero_for_multiplo_somente_de_5(self):
+        entradas = (5, 10, 20, 25)
+        saida = 'Ovos'
+        
+        for entrada in entradas:
+            with self.subTest(entrada=entrada, saida=saida):
+                self.assertEqual(
+                    bacon_com_ovos(entrada),
+                    saida,
+                    msg = f'"{entrada}" não retornou "{saida}"'
+                )
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
